67e9066a64220322b1be0a7d39c1f7442aa832f4
kevinhuangwl <kevinbauer@163.com>
2020-03-17 20:57:58 +0800

    更新持仓计算

--- northstar-gateway-impl-simulate/src/main/java/tech/xuanwu/northstar/GwPosition.java
+++ northstar-gateway-impl-simulate/src/main/java/tech/xuanwu/northstar/GwPosition.java
@@ -1,21 +1,25 @@
-package tech.xuanwu.northstar.domain;
+package tech.xuanwu.northstar;
 
 import org.apache.commons.codec.binary.StringUtils;
 
 import tech.xuanwu.northstar.entity.ContractInfo;
 import tech.xuanwu.northstar.entity.PositionInfo;
 import tech.xuanwu.northstar.exception.ContractMismatchException;
+import tech.xuanwu.northstar.exception.TradeException;
+import xyz.redtorch.pb.CoreEnum.OffsetFlagEnum;
 import xyz.redtorch.pb.CoreEnum.PositionDirectionEnum;
 import xyz.redtorch.pb.CoreField.PositionField;
 import xyz.redtorch.pb.CoreField.TickField;
 import xyz.redtorch.pb.CoreField.TradeField;
 
 /**
- * 持仓对象领域模型
+ * 持仓对象领域模型，专门用于持仓实时计算
+ * 本类仅限于模拟网关包内使用
+ * 同步锁设计为轻量级锁，因为模拟网关理论上只会不断接收tick事件线程的更新，偶尔会接收到挂单与撤单的接口线程调用
  * @author kevinhuangwl
  *
  */
-public class GwPosition {
+class GwPosition {
 	
 	private PositionInfo p;
 	
@@ -33,7 +37,7 @@ public class GwPosition {
 	 * 加仓
 	 * @param tradeField
 	 */
-	public GwPosition addPosition(TradeField tradeField) {
+	public PositionField addPosition(TradeField tradeField) {
 		String expectSymbol = p.getContract().getUnifiedSymbol();
 		String actualSymbol = tradeField.getContract().getUnifiedSymbol();
 		if(!StringUtils.equals(expectSymbol, actualSymbol)) {
@@ -50,8 +54,8 @@ public class GwPosition {
 			double nCost = tradeField.getPrice() * tradeField.getVolume() * contract.getMultiplier();
 			double openCost = p.getOpenPrice() * p.getPosition() * contract.getMultiplier();
 			
-			p.setPosition(oPosition + nPosition);
 			p.setTdPosition(tdPosition + nPosition);
+			p.setPosition(oPosition + nPosition);
 			p.setPrice((cost + nCost) / (p.getPosition() * contract.getMultiplier()));
 			p.setOpenPrice((openCost + nCost) / (p.getPosition() * contract.getMultiplier()));
 			p.setContractValue(tradeField.getPrice() * p.getPosition() * contract.getMultiplier());
@@ -59,14 +63,14 @@ public class GwPosition {
 			p.setUseMargin(p.getExchangeMargin());
 		}
 		
-		return this;
+		return p.convertTo();
 	}
 	
 	/**
 	 * 减仓
 	 * @param tradeField
 	 */
-	public GwPosition reducePosition(TradeField tradeField) {
+	public PositionField reducePosition(TradeField tradeField) {
 		String expectSymbol = p.getContract().getUnifiedSymbol();
 		String actualSymbol = tradeField.getContract().getUnifiedSymbol();
 		if(!StringUtils.equals(expectSymbol, actualSymbol)) {
@@ -75,21 +79,28 @@ public class GwPosition {
 		
 		synchronized (p) {			
 			int nPosition = tradeField.getVolume();
-			int oPosition = p.getPosition();
-			int tdPosition = p.getTdPosition();
 
-			p.setPosition(oPosition - nPosition);
-			p.setTdPosition(tdPosition - nPosition);
+			if(tradeField.getOffsetFlag()==OffsetFlagEnum.OF_CloseToday) {
+				p.setTdPosition(p.getTdPosition() - nPosition);	
+				p.setPosition(p.getPosition() - nPosition);
+			}else if(tradeField.getOffsetFlag()==OffsetFlagEnum.OF_CloseYesterday) {
+				p.setYdPosition(p.getYdPosition() - nPosition);
+				p.setPosition(p.getPosition() - nPosition);
+			}else {
+				p.setPosition(p.getPosition() - nPosition);
+				p.setTdPosition(p.getTdPosition() - Math.min(p.getTdPosition(), nPosition));
+				p.setYdPosition(p.getPosition() - p.getTdPosition());
+			}
 		}
 		
-		return this;
+		return p.convertTo();
 	}
 	
 	/**
 	 * 行情更新
 	 * @param tick
 	 */
-	public void updateByTick(TickField tick) {
+	public PositionField updateByTick(TickField tick) {
 		String expectSymbol = p.getContract().getUnifiedSymbol();
 		String actualSymbol = tick.getUnifiedSymbol();
 		if(!StringUtils.equals(expectSymbol, actualSymbol)) {
@@ -107,31 +118,25 @@ public class GwPosition {
 			
 			p.setContractValue(tick.getLastPrice() * p.getPosition() * contract.getMultiplier());
 		}
+		return p.convertTo();
 	}
 	
 	/**
 	 * 日结算
 	 */
-	public void proceedDailySettlement() {
+	public PositionField proceedDailySettlement() {
 		synchronized (p) {
-			
+			p.setYdPosition(p.getYdPosition() + p.getTdPosition());
 		}
+		return p.convertTo();
 	}
+	
 	
 	/**
 	 * 获取持仓信息
 	 * @return
 	 */
-	public PositionInfo getPositionInfo() {
-		//防止对象被外部修改
-		return PositionInfo.convertFrom(p.convertTo());
-	}
-	
-	/**
-	 * 获取持仓信息
-	 * @return
-	 */
-	public PositionField getPositionField() {
+	public PositionField getPosition() {
 		return p.convertTo();
 	}
 	
