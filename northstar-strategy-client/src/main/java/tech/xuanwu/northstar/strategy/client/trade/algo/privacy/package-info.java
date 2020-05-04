befd7073f36a431e69eaae7978af78575d8c30ae
kevinhuangwl <kevinbauer@163.com>
2020-05-04 21:28:03 +0800

    增加隐私文件夹用于存放私有文件

---
+++ northstar-strategy-client/src/main/java/tech/xuanwu/northstar/strategy/client/trade/algo/privacy/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 该目录下存放不公开的算法。除了包说明文件，其余所有的JAVA文件将不会被提交
+ */
+/**
+ * @author kevinhuangwl
+ *
+ */
+package tech.xuanwu.northstar.strategy.client.trade.algo.privacy;
---
+++ northstar-strategy-client/src/main/java/tech/xuanwu/northstar/strategy/client/trade/strategy/privacy/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 该目录下存放不公开的策略。除了包说明文件，其余所有的JAVA文件将不会被提交
+ */
+/**
+ * @author kevinhuangwl
+ *
+ */
+package tech.xuanwu.northstar.strategy.client.trade.strategy.privacy;
