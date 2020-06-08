0a274d9c25069c20504929a926950bb4b13b902d
kevinhuangwl <kevinbauer@163.com>
2020-06-08 20:39:51 +0800

    tick数据按symbol隔离分发

--- northstar-core/src/main/java/tech/xuanwu/northstar/core/engine/SocketIOMessageEngine.java
+++ northstar-core/src/main/java/tech/xuanwu/northstar/core/engine/SocketIOMessageEngine.java
@@ -7,6 +7,7 @@ import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.annotation.OnConnect;
 import com.corundumstudio.socketio.annotation.OnDisconnect;
+import com.corundumstudio.socketio.annotation.OnEvent;
 
 import lombok.extern.slf4j.Slf4j;
 import tech.xuanwu.northstar.constant.Message;
@@ -25,9 +26,7 @@ public class SocketIOMessageEngine implements MessageEngine{
 	
 	@Override
 	public void emitTick(TickField tick) {
-		for(SocketIOClient client : server.getAllClients()) {
-			client.sendEvent(Message.MARKET_DATA, tick.toByteArray());
-		}
+		server.getRoomOperations(tick.getUnifiedSymbol()).sendEvent(Message.MARKET_DATA, tick.toByteArray());
 	}
 	
 	
@@ -44,4 +43,15 @@ public class SocketIOMessageEngine implements MessageEngine{
     	log.info("【客户端断开】-[{}],断开连接", client.getSessionId());
     }
     
+    @OnEvent("login")
+    private void login(final SocketIOClient client, String room) {
+    	log.info("【登陆房间】-[{}]加入房间{}", client.getSessionId(), room);
+    	client.joinRoom(room);
+    }
+    
+    @OnEvent("logout")
+    private void logout(final SocketIOClient client, String room) {
+    	log.info("【离开房间】-[{}]离开房间{}", client.getSessionId(), room);
+    	client.leaveRoom(room);
+    }
 }
